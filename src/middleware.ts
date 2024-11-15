8c3c6b7ef53bacd450c6a490a56fa4f6a10018ee
tranmanh03 <tranmanh200305@gmail.com>
2024-11-15 08:57:25 +0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,26 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { store } from "./store/store";
 
-const privatePaths = ["/me"];
+const privatePaths = ["/me", "/admin", "/manage-user"];
 const authPaths = ["/login", "/register"];
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    // const { pathname } = request.nextUrl;
-    // const token = store?.getState()?.auth.account?.token;
-    // console.log(token);
+    const { pathname } = request.nextUrl;
+    const token = request.headers.get("Authorization");
 
-    // if (privatePaths.some((path) => pathname.startsWith(path)) && !token) {
-    //     return NextResponse.redirect(new URL("/login", request.url));
-    // }
+    if (privatePaths.some((path) => pathname.startsWith(path)) && !token) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-    // if (authPaths.some((path) => pathname.startsWith(path)) && token) {
-    //     return NextResponse.redirect(new URL("/", request.url));
-    // }
+    if (authPaths.some((path) => pathname.startsWith(path)) && token) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
     return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
-// export const config = {
-//     matcher: ["/login", "/register", "/me"],
-// };
+export const config = {
+    matcher: [...privatePaths, authPaths],
+};
